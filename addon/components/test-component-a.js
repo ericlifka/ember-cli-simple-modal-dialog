90b3b59b24735157c9c4346752f88b80e21af364
Eric Lifka <eric.lifka@gmail.com>
2015-10-12 23:01:40 -0400

    apparently i need to import templates manually...

--- addon/components/test-component-a.js
+++ addon/components/test-component-a.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import layout from '../templates/components/test-component-a';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['test-component-a']
+  classNames: ['test-component-a'],
+  layout: layout
 });
