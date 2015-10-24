0f254b89493b824cf759d9871194e06a545b72b3
Eric Lifka <eric.lifka@gmail.com>
2015-10-23 22:57:40 -0400

    add logic to the overlay to allow it to cancel the modal when a dismiss action is provided

--- addon/components/simple-modal-dialog/component.js
+++ addon/components/simple-modal-dialog/component.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
+import CancelModalMixin from './mixins/cancel-modal';
 import layout from './template';
 const { Component } = Ember;
 
-export default Component.extend({
+export default Component.extend(CancelModalMixin, {
   classNames: [ 'SMD-simple-modal-dialog' ],
-  layout: layout
+  layout: layout,
+
+  actions: {
+    clickToCancel() {
+      if (this.get('showDismiss')) {
+        this.send('dismiss');
+      }
+    }
+  }
 });
